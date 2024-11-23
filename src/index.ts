6d4c1ccce3c9b3bb9b2c3bff7006496cb063d432
Daniel Parnell <me@danielparnell.com>
2024-11-23 10:54:12 +0000

    fixed platform name

--- src/index.ts
+++ src/index.ts
@@ -68,7 +68,7 @@ class NodeTLSSocket extends Duplex {
 /**
  * This is the name of the platform that users will use to register the plugin in the Homebridge config.json
  */
-export const PLATFORM_NAME = 'SamsungCac';
+export const PLATFORM_NAME = 'SamsungCacModern';
 
 /**
  * This must match the name of your plugin as defined the package.json
